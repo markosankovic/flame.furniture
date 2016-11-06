24864594a4d71e020e7f7d48277b52de9e6e6e50
Marko Sanković <sankovic.marko@gmail.com>
2016-11-07 00:13:01 +0100

    Send contact email

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ flame.furniture
 
    run the app:
      $ DEBUG=flame.furniture:* npm start
+
+   run the develop app:
+     $ DEBUG=flame.furniture:* NODEMAILER_TRANSPORTER=smtps://flamefurniture%40gmail.com:password@smtp.gmail.com MAIL_TO=foo@bar.com nodemon
+
+   contact send test:
+     $ curl -i --data "name=Foo&email=foo@bar.com&subject=Test&message=Message" http://localhost:3000/contact/send
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var mailer = require("nodemailer");
+
+var debug = require('debug')('flame.furniture:server');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -24,6 +27,30 @@ router.get('/gallery', function (req, res, next) {
 /* GET home page. */
 router.get('/contact', function (req, res, next) {
   res.render('contact', { title: 'CONTACT - FLAME Furniture Inc.' });
+});
+
+/* POST contact send. */
+router.post('/contact/send', function (req, res, next) {
+
+  var transporter = mailer.createTransport(process.env.NODEMAILER_TRANSPORTER);
+
+  var mail = {
+    from: "FLAME Furniture Inc. <flamefurniture@gmail.com>",
+    to: process.env.MAIL_TO,
+    subject: "flame.furniture: " + req.body.subject,
+    text: req.body.name + " <" + req.body.email + ">\n\n" + req.body.message
+  };
+
+  transporter.sendMail(mail, function (error, response) {
+    if (error) {
+      debug(error);
+      res.status(500).json({ success: false });
+    } else {
+      debug(response);
+      res.json({ success: true });
+    }
+    transporter.close();
+  });
 });
 
 module.exports = router;
