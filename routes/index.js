7eab32885efe3f64563af10d666cce72297f15ea
Marko Sanković <sankovic.marko@gmail.com>
2016-11-06 17:28:54 +0100

    Change titles

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'FLAME FURNITURE INC.' });
+  res.render('index', { title: 'FLAME Furniture Inc.' });
 });
 
 /* GET about page. */
 router.get('/about', function (req, res, next) {
-  res.render('about', { title: 'ABOUT :: FLAME FURNITURE INC.' });
+  res.render('about', { title: 'ABOUT - FLAME Furniture Inc.' });
 });
 
 /* GET shop page. */
 router.get('/shop', function (req, res, next) {
-  res.render('shop', { title: 'SHOP :: FLAME FURNITURE INC.' });
+  res.render('shop', { title: 'SHOP - FLAME Furniture Inc.' });
 });
 
 /* GET gallery page. */
 router.get('/gallery', function (req, res, next) {
-  res.render('gallery', { title: 'GALLERY :: FLAME FURNITURE INC.' });
+  res.render('gallery', { title: 'GALLERY - FLAME Furniture Inc.' });
 });
 
 /* GET home page. */
 router.get('/contact', function (req, res, next) {
-  res.render('contact', { title: 'CONTACT :: FLAME FURNITURE INC.' });
+  res.render('contact', { title: 'CONTACT - FLAME Furniture Inc.' });
 });
 
 module.exports = router;
