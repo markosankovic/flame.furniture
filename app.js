18bdf3ad62f0230760a576cd7b16eaf0fe8406ad
Marko Sanković <sankovic.marko@gmail.com>
2016-11-26 04:56:42 +0100

    Install and use eslint

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -34,7 +34,7 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
