2e7aeb6a2e991cda5d6ba7fc1a9d592c2b759184
Marko Sanković <sankovic.marko@gmail.com>
2016-11-28 11:29:50 +0100

    Change the order build and stop/remove Docker container

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 git pull
-docker stop flame.furniture && docker rm flame.furniture
 docker build -t markosankovic/flame.furniture .
+docker stop flame.furniture && docker rm flame.furniture
 docker run -d -p 80:3000 --name=flame.furniture markosankovic/flame.furniture
