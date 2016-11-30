9715667c778fcb40bb148c55b9fb01a44c80684e
Marko Sanković <sankovic.marko@gmail.com>
2016-11-30 20:28:27 +0100

    Install and configure additional packages: mongoldb, mocha, doting

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var expressLayouts = require('express-ejs-layouts');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
-var app = express();
+require('dotenv').config(); // load environment variables from a .env file into process.env
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,17 +25,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// connect to mongodb
+mongoose.Promise = global.Promise; // use native promises
+mongoose.connect(process.env.MONGOOSE_CONNECT_URI);
+
 app.use(require('./controllers'));
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function (err, req, res) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
